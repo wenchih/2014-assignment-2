e0dfb5781891b6f97c049af9ea28187e5cd7d368
wen-chih  tseng <102753023@nccu.edu.tw>
2014-05-18 16:19:57 +0800

    002

---
+++ nccucs-assignment-2/graph/Graph.java
@@ -0,0 +1,53 @@
+package ps3.graph;
+
+public class Graph {
+	
+	public String Name (String name)
+	{
+	 return name;	
+	}
+	
+	public String CreateGraph(String name)
+	{
+		Graph name;	
+		return "Graph "+name+" is Create ";
+	}
+	
+	public String CreateNode(String node,int cost)
+	{
+		
+	}
+	
+	public String AddNode(Graph X,Node Y)
+	{
+		
+	}
+	
+	public String ListNodes(Graph A)
+	{
+		
+	}
+	public String ListEdges(Graph X)
+	{
+		
+	}
+	public String ListChildren(Graph X,Node Y)
+	{
+		
+	}
+	public String FindPath(Graph X,Node Y,Node Z)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
